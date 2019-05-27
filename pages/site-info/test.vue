d86d5141bf1ae77c024fe326a7bb617b7b86fc45
zhoumengyu <zhoumengyu@sfere-elec.com>
2019-05-27 16:11:50 +0800

    服了 败给ucharts了

--- pages/site-info/test.vue
+++ pages/site-info/test.vue
@@ -1,62 +1,162 @@
 <template>
-	<view>
-		<line-chart :chartData="chartData"></line-chart>
-		<line-chart :chartData="chartData1"></line-chart>
-		<button type="primary" @click="upData()">updata</button>
+	<view class="qiun-columns">
+		<view class="qiun-bg-white qiun-title-bar qiun-common-mt">
+			<view class="qiun-title-dot-light">柱状图</view>
+		</view>
+		<view class="qiun-charts">
+			<canvas canvas-id="canvasColumn" id="canvasColumn" class="charts"></canvas>
+		</view>
 	</view>
 </template>
 
 <script>
-	import lineChart from '@/components/u-charts/line-chart.vue'
+	import uCharts from '../../components/u-charts/u-charts.js';
+	var _self;
+	var canvaColumn = null;
 	export default {
-		name: 'test',
-		components: {
-			lineChart
-		},
 		data() {
 			return {
-				canvasId: 'test',
-				chartData: {
-					categories: ["2012", "2013", "2014", "2015", "2016", "2017"],
-					series: [{
-						name: "成交量A",
-						data: [35, 8, 25, 37, 4, 20]
-					}, {
-						name: "成交量B",
-						data: [70, 40, 65, 100, 44, 68]
-					}, {
-						name: "成交量C",
-						data: [100, 80, 95, 150, 112, 132]
-					}]
-				},
-				chartData1: {
-					categories: ["2012", "2013", "2014", "2015", "2016", "2017"],
-					series: [{
-						name: "成交量A",
-						data: [35, 421,125,32, 4, 20]
-					}, {
-						name: "成交量B",
-						data: [70, 40, 65, 512, 44, 68]
-					}, {
-						name: "成交量C",
-						data: [100, 80, 95, 21, 32, 132]
-					}]
-				}
+				cWidth: '',
+				cHeight: '',
+				pixelRatio: 1,
+				serverData: ''
 			}
 		},
-		methods: {
-			upData() {
-				let dataList = {
-					name: "成交量D",
-					data: [22, 33, 11, 44, 22, 33]
+		onLoad() {
+			_self = this;
+			//#ifdef MP-ALIPAY
+			uni.getSystemInfo({
+				success: function(res) {
+					if (res.pixelRatio > 1) {
+						//正常这里给2就行，如果pixelRatio=3性能会降低一点
+						//_self.pixelRatio =res.pixelRatio;
+						_self.pixelRatio = 2;
+					}
 				}
-				this.chartData.series.push(dataList)
-				console.log(this.chartData)
+			});
+			//#endif
+			this.cWidth = uni.upx2px(750);
+			this.cHeight = uni.upx2px(500);
+			this.getServerData();
+		},
+		methods: {
+			getServerData() {
+				uni.request({
+					url: 'https://www.easy-mock.com/mock/5cc586b64fc5576cba3d647b/uni-wx-charts/chartsdata2',
+					data: {},
+					success: function(res) {
+						console.log(res.data.data)
+						//下面这个根据需要保存后台数据，我是为了模拟更新柱状图，所以存下来了
+						_self.serverData = res.data.data;
+						let Column = {
+							categories: [],
+							series: []
+						};
+						//这里我后台返回的是数组，所以用等于，如果您后台返回的是单条数据，需要push进去
+						Column.categories = res.data.data.Column.categories;
+						//这里的series数据是后台做好的，如果您的数据没有和前面我注释掉的格式一样，请自行拼接数据
+						Column.series = res.data.data.Column.series;
+						_self.showColumn("canvasColumn", Column);
+					},
+					fail: () => {
+						console.log("网络错误，小程序端请检查合法域名");
+					},
+				});
+			},
+			showColumn(canvasId, chartData) {
+				canvaColumn = new uCharts({
+					$this: _self,
+					canvasId: canvasId,
+					type: 'line',
+					legend: true,
+					fontSize: 11,
+					background: '#FFFFFF',
+					pixelRatio: _self.pixelRatio,
+					animation: true,
+					categories: chartData.categories,
+					series: chartData.series,
+					xAxis: {
+						disableGrid: true,
+					},
+					yAxis: {},
+					dataLabel: true,
+					width: _self.cWidth * _self.pixelRatio,
+					height: _self.cHeight * _self.pixelRatio,
+					extra: {
+						column: {
+							width: _self.cWidth * _self.pixelRatio * 0.45 / chartData.categories.length
+						}
+					}
+				});
+			},
+			changeData() {
+				canvaColumn.updateData({
+					series: _self.serverData.ColumnB.series,
+					categories: _self.serverData.ColumnB.categories
+				});
 			}
 		}
 	}
 </script>
 
-<style scoped>
+<style>
+	page {
+		background: #F2F2F2;
+		width: 750upx;
+		overflow-x: hidden;
+	}
 
+	.qiun-padding {
+		padding: 2%;
+		width: 96%;
+	}
+
+	.qiun-wrap {
+		display: flex;
+		flex-wrap: wrap;
+	}
+
+	.qiun-rows {
+		display: flex;
+		flex-direction: row !important;
+	}
+
+	.qiun-columns {
+		display: flex;
+		flex-direction: column !important;
+	}
+
+	.qiun-common-mt {
+		margin-top: 10upx;
+	}
+
+	.qiun-bg-white {
+		background: #FFFFFF;
+	}
+
+	.qiun-title-bar {
+		width: 96%;
+		padding: 10upx 2%;
+		flex-wrap: nowrap;
+	}
+
+	.qiun-title-dot-light {
+		border-left: 10upx solid #0ea391;
+		padding-left: 10upx;
+		font-size: 32upx;
+		color: #000000
+	}
+
+	/* 通用样式 */
+	.qiun-charts {
+		width: 750upx;
+		height: 500upx;
+		background-color: #FFFFFF;
+	}
+
+	.charts {
+		width: 750upx;
+		height: 500upx;
+		background-color: #FFFFFF;
+	}
 </style>
